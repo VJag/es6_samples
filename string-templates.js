fd816feaa8026db4d367cced164c9c5a78d90611
VJag <jagannadh81@gmail.com>
2016-09-15 14:18:19 +0530

    Adding string template demo

--- string-templates.js
+++ string-templates.js
@@ -1,3 +1,26 @@
-/**
- * Created by jvanguri on 9/15/2016.
- */
+var userName = "Brooks";
+
+// Old way
+var welcomeMessage = "Welcome " + userName;
+console.log(welcomeMessage);
+
+// ES6 way
+var newWelcomeMessage = `Welcome ${userName}`;
+console.log(welcomeMessage);
+
+// ES6 tagged template literals
+var gender = "Male";
+function getFormattedWelcomeMessage(strings, ...values) {
+    let salutation = "Ms";
+    if(gender === "Male") {
+        salutation = "Mr";
+    }
+    values[0] = salutation;
+
+    return `${strings[0]} ${values[0]}.${values[1]}`;
+}
+
+newWelcomeMessage = getFormattedWelcomeMessage`Welcome ${""} ${userName}`;
+console.log(newWelcomeMessage);
+
+
